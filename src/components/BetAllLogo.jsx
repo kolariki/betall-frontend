17fe741ceaa71e8ce82c3958dcceb04d8855aead
ivankolariki@gmail.com <ivan@nubimetrics.com>
2026-03-10 23:11:16 -0300

    svg

--- src/components/BetAllLogo.jsx
+++ src/components/BetAllLogo.jsx
@@ -1,9 +1,3 @@
-export default function BetAllLogo({ className = "w-8 h-8" }) {
-  return (
-    <img 
-      src="/betall-logo.svg" 
-      alt="BetAll" 
-      className={className}
-    />
-  );
+export default function BetAllLogo({ className = "w-8 h-8 mb-10" }) {
+  return <img src="/betall-logo.svg" alt="BetAll" className={className} />;
 }
