71454f437c0070f16c9360153bf88aeb01cf9349
kolariki <kolariki@users.noreply.github.com>
2026-03-10 21:34:53 -0300

    Add missing BetAllLogo.jsx component

---
+++ src/components/BetAllLogo.jsx
@@ -0,0 +1,9 @@
+export default function BetAllLogo({ className = "w-8 h-8" }) {
+  return (
+    <img 
+      src="/betall-logo.svg" 
+      alt="BetAll" 
+      className={className}
+    />
+  );
+}
